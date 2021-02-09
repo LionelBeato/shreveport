226d096cb0764e207c606a9b14d8841ea3ff2b88
Lionel Beato <lionel.beato@gmail.com>
2021-02-09 12:00:55 -0500

    adds authentication projects

---
+++ spring/auth/oauthdemo/src/test/java/com/tts/oauthdemo/OauthdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.oauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
