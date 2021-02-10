d55055d6486e3b2b7d8d01ae3306335eb818b974
Lionel Beato <lionel.beato@gmail.com>
2021-02-10 12:15:05 -0500

    starts spring blog project

--- spring/techtalentblog/src/main/java/com/tts/techtalentblog/controller/BlogPostController.java
+++ spring/techtalentblog/src/main/java/com/tts/techtalentblog/controller/BlogPostController.java
@@ -1,11 +1,26 @@
 package com.tts.techtalentblog.controller;
 
 import com.tts.techtalentblog.model.BlogPost;
+import com.tts.techtalentblog.repo.BlogPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlogPostController {
+
+    // @Autowired allows us to implement whats known as dependency injection
+    // Dependency injection allows us to give certain objects the dependencies that it needs
+//    @Autowired
+    private BlogPostRepository blogPostRepository;
+
+    // below is a constructor based dependency injection
+    // if you only have one dependency, this is considered best practice
+    public BlogPostController(BlogPostRepository blogPostRepository ) {
+        this.blogPostRepository = blogPostRepository;
+    }
 
     @GetMapping("/")
     public String index(BlogPost blogPost) {
@@ -15,5 +30,18 @@ public class BlogPostController {
         // will allow us to show the data that we want
         return "blogpost/index";
     }
+
+    // this is where we are mapping our post request in our project
+    @PostMapping("/")
+    public String addNewBlogPost(BlogPost blogPost, Model model) {
+        // blogPost from our parameter is the object we're getting from
+        // the thymeleaf form, we can simple save it in our repository
+        blogPostRepository.save(blogPost);
+        model.addAttribute("title",blogPost.getTitle());
+        model.addAttribute("author",blogPost.getAuthor());
+        model.addAttribute("blogEntry",blogPost.getBlogEntry());
+        return "blogpost/result";
+    }
+
 
 }
