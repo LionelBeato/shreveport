44fe6aae6f1447dd93f498a7b23bbdf0d837de0c
Lionel Beato <lionel.beato@gmail.com>
2021-02-16 09:52:06 -0500

    adds user model to twitter project

---
+++ spring/demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int active;
+
+    @CreationTimestamp
+    private Date createdAt;
+}
