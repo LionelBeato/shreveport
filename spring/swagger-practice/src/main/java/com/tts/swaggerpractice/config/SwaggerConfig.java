ecc4cc6c158905bc8a5deb63f71ad0d18cb810ee
Lionel Beato <lionel.beato@gmail.com>
2021-03-03 10:45:50 -0500

    adds swagger project

---
+++ spring/swagger-practice/src/main/java/com/tts/swaggerpractice/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.tts.swaggerpractice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.tts.swaggerpractice"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Menu Items API")
+                .description("REST API for interacting with menu items")
+                .version("1.0.0")
+                .contact(new Contact("Developer name", "website.com", "developer@website.com"))
+                .license("Apache License Version 2.0")
+                .license("https://www.apache.org/licenses/LICENSE-2.0")
+                .build();
+    }
+
+}
---
+++ spring/swagger-practice/src/test/java/com/tts/swaggerpractice/SwaggerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.swaggerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
